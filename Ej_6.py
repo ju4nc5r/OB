214c5225f9b85d316f9eaaf6d02d2c2c54a18223
ju4nc5r <31509271+ju4nc5r@users.noreply.github.com>
2023-06-13 23:00:38 -0300

    Completo ej6

---
+++ Ej_6.py
@@ -0,0 +1,14 @@
+class Vehiculo:
+    color = "Rojo"
+    ruedas = 4
+    puertas = 4
+
+class Coche:
+
+    velocidad = 60
+    cilindrada = 180
+
+coche = Coche()
+vel_coche = coche.velocidad
+
+print(vel_coche)
