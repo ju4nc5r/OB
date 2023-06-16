9f710efa5ff2e1bec5c952f4e63317b8d6fa391e
ju4nc5r <31509271+ju4nc5r@users.noreply.github.com>
2023-06-16 20:25:49 -0300

    Completo ej8

---
+++ Ej_8/calc_use.py
@@ -0,0 +1,12 @@
+from Ej_8.calculadora import Calc
+
+
+Calcu = Calc()
+
+a = 10
+b = 20
+
+Calcu.suma(a,b)
+Calcu.resta(a,b)
+Calcu.multiplicacion(a,b)
+Calcu.division(a,b)
---
+++ Ej_8/calculadora.py
@@ -0,0 +1,19 @@
+class Calc:
+
+    def suma(self, a, b):
+        suma = a + b
+        print('El valor de la suma es: ' + str(suma))
+
+    def resta(self, a, b):
+        resta = a - b
+        print('El valor de la resta es: ' + str(resta))
+
+    def multiplicacion(self, a, b):
+        mult = a * b
+        print('El valor de la multiplicacion es: ' + str(mult))
+
+    def division(self, a, b):
+        div = a / b
+        print('El valor de la division es: ' + str(div))
+
+
