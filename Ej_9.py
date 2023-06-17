948464d9ebbd5376b322d66a1c01306ab974f91d
ju4nc5r <31509271+ju4nc5r@users.noreply.github.com>
2023-06-16 21:02:44 -0300

    Completo ej9

--- Ej_9.py
+++ Ej_9.py
@@ -2,14 +2,15 @@ import time
 
 hora = time.time()
 horaActual = time.strftime("%H:%M:%S", time.localtime(hora))
+minutos = time.strftime('%M')
 hora_format = time.localtime().tm_hour
-
+#hora_format = 9
 print('Actualmente son las: ' + str(horaActual))
 if hora_format >= 19:
     print('Es hora de ir a casa')
 
 else:
     restante = 19 - hora_format
-    print('El tiempo restante de trabajo son: ' + str(restante),' hs')
+    print('El tiempo de trabajo restante son:',str(restante),':',str(minutos))
 
 
